641ba79af3afbbb09bd04fdfd7c8d3a3eea35c3d
vpj-franciscojavierBB <fjbecerrob02@informatica.iesvalledeljerteplasencia.es>
2025-10-16 15:42:34 +0200

    16/10/2025

---
+++ TEMA 3/Ejercicio11-T3/src/ejercicio11/t3/Ejercicio11T3.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio11.t3;
+
+/**
+ *
+ * @author Jav1e7
+ */
+public class Ejercicio11T3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;    
+        
+        for (i = 0; i <= 6; i++) {
+            System.out.print("Hola" + i);
+            if (i < 6) {
+                System.out.print(" - ");
+            }
+        }
+        System.out.println(); // Salto de línea final
+    }
+}        
+   
+        // TODO code application logic here
+    
+    
+
+   
