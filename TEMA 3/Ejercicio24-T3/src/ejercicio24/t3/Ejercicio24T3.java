7c525702a509840247593267cf0c8d8fb8da9df0
vpj-franciscojavierBB <fjbecerrob02@informatica.iesvalledeljerteplasencia.es>
2025-10-17 16:39:48 +0200

    17/10/202

---
+++ TEMA 3/Ejercicio24-T3/src/ejercicio24/t3/Ejercicio24T3.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio24.t3;
+import java.util.Scanner;
+/**
+ *
+ * @author Jav1e7
+ */
+public class Ejercicio24T3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        int n1 = 1;
+        int n2;
+        
+        System.out.println("Introduce el numero: ");
+        n2 = entrada.nextInt();
+        
+        do(
+        
+            if (n1 % 3 == 0)
+                System.err.print(n1 + "-");
+            n1++;
+        );
+        while
+
+        
+
+// TODO code application logic here
+    }
+    
+}
