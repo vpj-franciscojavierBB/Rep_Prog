af6ec7100952341721aa8f5f26bad5e8d163ed1c
vpj-franciscojavierBB <fjbecerrob02@informatica.iesvalledeljerteplasencia.es>
2025-10-27 19:33:12 +0100

    Ejercicio 2

--- TEMA 3/Ejercicio2-T4/src/ejercicio2/t4/Ejercicio2T4.java
+++ TEMA 3/Ejercicio2-T4/src/ejercicio2/t4/Ejercicio2T4.java
@@ -3,34 +3,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio2.t4;
-import java.util.Scanner;
-import ejercicio2.t4.suma; 
+
+import java.util.Scanner;       // Importamos Scanner para leer datos del usuario
+import ejercicio2.t4.suma;       // Importamos la clase suma que contiene los métodos suma y producto
+
 /**
  *
  * @author Jav1e7
  */
 public class Ejercicio2T4 {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        float n1, n2;
+        
+        Scanner entrada = new Scanner(System.in); // Creamos el objeto Scanner para capturar la entrada del usuario
+        
+        float n1, n2; // Declaramos las variables donde guardaremos los números ingresados por el usuario
 
         System.out.print("Por favor, introduzca un numero: ");
-        n1 = entrada.nextFloat();
+        n1 = entrada.nextFloat(); // Leemos y guardamos el primer número
 
         System.out.print("Ahora, introduzca un segundo numero: ");
-        n2 = entrada.nextFloat();
+        n2 = entrada.nextFloat(); // Leemos y guardamos el segundo número
 
-        float resultado;
-        String operacion;
+        float resultado; // Variable para almacenar el resultado de la operación
+        String operacion; // Variable para saber qué operación se hizo
 
+        // Si el primer número es mayor que 10, hacemos la operación producto
         if (n1 > 10) {
-            resultado = suma.producto(n1, n2);
+            resultado = suma.producto(n1, n2); // Llamamos al método producto de la clase suma
             operacion = "producto";
         } else {
-            resultado = suma.suma(n1, n2);
+            // Si no, realizamos una suma
+            resultado = suma.suma(n1, n2); // Llamamos al método suma de la clase suma
             operacion = "suma";
         }
 
-        System.out.println("La operación que se realizó es " + operacion + " y el resultado es " + resultado);
+        // Imprimimos el resultado
+        System.out.println("La operación que se realizó es: " + operacion + " y el resultado es: " + resultado);
     }
 }
--- TEMA 3/Ejercicio2-T4/src/ejercicio2/t4/suma.java
+++ TEMA 3/Ejercicio2-T4/src/ejercicio2/t4/suma.java
@@ -12,13 +12,16 @@
 
 package ejercicio2.t4;
 
+// Clase llamada "suma" que contiene dos métodos estáticos
 public class suma {
+
+    // Método estático que recibe dos números y devuelve su suma
     public static float suma(float n1, float n2) {
-        return n1 + n2;
+        return n1 + n2; // Retorna el resultado de la suma
     }
 
+    // Método estático que recibe dos números y devuelve su producto
     public static float producto(float n1, float n2) {
-        return n1 * n2;
+        return n1 * n2; // Retorna el resultado de la multiplicación
     }
 }
-
